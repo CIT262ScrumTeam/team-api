7a350d55c86000138f5129fccc3ebc7277888b14
mattmann24 <mdshank24@gmail.com>
2023-10-25 18:39:16 -0600

    Matts commit

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ app.get('/josh', (req,res) => {res.send("Hello Josh")});
 
 app.get('/walter', (req,res) => {res.send("Hello Walter")});
 
+app.get('/matt', (req,res) => {res.send("Hello Matt")}); 
+
+
 app.listen(port, ()=>console.log("Listening"));

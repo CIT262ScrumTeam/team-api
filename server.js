280e9e9cd0e8d7aaa25d97e066a0227e2b4bdec8
Walter Taylor <walterht@byui.edu>
2023-10-25 18:37:49 -0600

    Set up server and app

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+
+const port = 3000;
+
+const app = express();
+
 app.get('/josh', (req,res) => {res.send("Hello Josh")}); 
+
+app.get('/walter', (req,res) => {res.send("Hello Walter")});
+
+app.listen(port, ()=>console.log("Listening"));

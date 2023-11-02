c8136c58f3d9ab731b92f347b578ccee9c0d48e5
Josh Dalton <joshmcdalton@gmail.com>
2023-11-01 20:12:17 -0400

    Added to Josh Test

--- test/josh.test.js
+++ test/josh.test.js
@@ -1,4 +1,28 @@
 
-const response = await fetch('http://localhost:3000/sean');
+const response = await fetch('http://localhost:3000/josh');
 const name = await response.text();
-expect.(name).toBe("josh");
+expect.(name).toBe("Hello Josh");
+
+// const fetch = require("node-fetch2");
+
+// it("Should get a login token",async ()=>{
+
+//     let token = "";//this is an emptry string
+//     const options = {
+//         method:"POST",
+//         headers:{
+//             "Content-Type":"application/json"
+//         },
+//         body:JSON.stringify({
+//             "userName":"scmurdock@gmail.com",
+//             "password":"P@ssw0rd"
+//         })
+//     }
+
+//     const response = await fetch('http://localhost:3000/josh');
+//     const name = await response.text();
+//     expect.(name).toBe("Hello Josh");
+//     token = await response.text();
+//     ;
+
+// })

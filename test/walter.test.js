561a8433273ebda5a2db4c71d7613c32e838306f
Walter Taylor <walterht@byui.edu>
2023-11-01 18:39:45 -0600

    Walter's test fixed

--- test/walter.test.js
+++ test/walter.test.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch2');
-
 it("Should return Hello Walter", async()=>{
     const response = await fetch('http://localhost:3000/walter');
     const reply = await response.text();
